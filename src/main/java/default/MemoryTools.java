088e7a788b2fa3775b138269546a4bea7d94d4df
Thomas <thomas.matecki@gmail.com>
2017-02-18 08:22:57 -0500

    Initial Commit

---
+++ src/main/java/default/MemoryTools.java
@@ -0,0 +1,10 @@
+
+import java.util.concurrent.TimeUnit;
+
+public class MemoryTools{
+      public static void main(String[] args) {
+
+            System.out.println("Waiting...");
+            while(1 == 1){}
+      } 
+} 
