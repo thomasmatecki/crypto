82f137981152110ce36f3115722002450ee58a34
Thomas <thomas.matecki@gmail.com>
2017-02-20 13:27:38 -0500

    Sha-256; Cautiously optimistic;

--- src/main/java/default/HashAlgorithms.java
+++ src/main/java/default/HashAlgorithms.java
@@ -1,21 +1,56 @@
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.nio.ByteBuffer;
-import java.math.BigInteger;
+import java.lang.String;
 
 public class HashAlgorithms {
 
   public static void main(String[] args) {
 
-    sha256("The Quick Brown Fox Jumbed over the lazy dog! Whille the cat watched and the dog remained asleep.");
+    for (String s : args) {
+      System.out.println(s);
+    }
 
+    //sha256("The Quick Brown Fox Jumbed over the lazy dog! Whille the cat watched and the dog remained asleep.");
+    sha256("");
+
+  }
+
+  private static void print512Bits(byte[] bytes) {
+
+    assert bytes.length == 64;
+
+    System.out.println('|' + new String(new char[77]).replace('\0', '-') + '|');
+
+    System.out.printf("| %5d bytes", bytes.length);
+
+    System.out.println(new String(new char[59]).replace('\0', ' ') + '|');
+
+    System.out.println('|' + new String(new char[77]).replace('\0', '-') + '|');
+
+    for (int i = 0; i < 8; i++) {
+
+      int bitFr = i * 8;
+      int bitTo = bitFr + 8;
+
+      //System.out.print("|" + bitFr + "|" + bitTo + "|");
+
+      System.out.printf("|%5d| %5d|", bitFr, bitTo);
+
+      for (int k = bitFr; k < bitTo; k++) {
+
+        String s1 = String.format("%8s", Integer.toBinaryString(bytes[k] & 0xFF)).replace(' ', '0');
+        System.out.print(s1);
+      }
+
+      System.out.println('|');
+
+    }
   }
 
   public static void sha256(String input) {
 
     /* Hash Values;  First 32 Bits of the fractional parts of the square roots of the first 8 primes */
-    final int[] H = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
+    int[] hash = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
 
     /* Round Constants; First 32 bits of the cube roots of the first 64 primes*/
     final int[] K = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -46,105 +81,105 @@ public class HashAlgorithms {
     byte[] paddedMsg = ByteBuffer.allocate(bufferSize).put(inputBytes).put(Byte.MIN_VALUE)
         .putLong(padLength + inputBytes.length + 1, inputBytes.length * Byte.SIZE).array();
 
-    System.out.println("The padded message is " + paddedMsg.length + " bytes");
+    //print512Bits(paddedMsg);
 
     for (int chnk = 0; chnk < paddedMsg.length; chnk += 64) {
 
       int[] w = new int[64];
 
-      /**
-       * System.out.println("-------------------------------------------------------------------");
-       * System.out.println("| BLOCK: " + chnk);
-       * System.out.println("-------------------------------------------------------------------");
-      */
+      print512Bits(Arrays.copyOfRange(paddedMsg, chnk, chnk + 64));
 
       for (int i = 0; i < 16; i++) {
-        /** 
-         * 0 0 ... 3
-         * 1 4 ... 7
-         * 2 8 ... 11
-         * .
-         * .
-         * .
-         * 15 60 ... 63
-         */
+
         int pf = chnk + i * 4;
         int pt = pf + 3;
 
         byte[] t = Arrays.copyOfRange(paddedMsg, pf, pt + 1);
 
-        System.out.print(
-            "POS: " + i + "; FROM: " + pf + "; TO: " + pt + " VAL: " + java.nio.ByteBuffer.wrap(t).getInt() + ";");
-
-        for (byte t0 : t) {
-          System.out.print(" " + t0);
-        }
-
         w[i] = java.nio.ByteBuffer.wrap(t).getInt();
 
-        System.out.println();
+        System.out.println("POS: " + i + "; FROM: " + pf + "; TO: " + pt + " VAL: " + w[i] + ";");
 
       }
 
-      /** for i from 16 to 63
-       *    s0 := (w[i-15] rightrotate 7) xor (w[i-15] rightrotate 18) xor (w[i-15] rightshift 3)
-       *    s1 := (w[i-2] rightrotate 17) xor (w[i-2] rightrotate 19) xor (w[i-2] rightshift 10)
-       *    w[i] := w[i-16] + s0 + w[i-7] + s1 */
-      for (int i = 16; i <= 63; i++) {
+      /**  Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array:
+       *   for i from 16 to 63
+       *     s0 := (w[i-15] rightrotate 7) xor (w[i-15] rightrotate 18) xor (w[i-15] rightshift 3)
+       *     s1 := (w[i-2] rightrotate 17) xor (w[i-2] rightrotate 19) xor (w[i-2] rightshift 10)
+       *     w[i] := w[i-16] + s0 + w[i-7] + s1 */
+      for (int i = 16; i < 64; i++) {
 
         int s0 = Integer.rotateRight(w[i - 15], 7) //
             ^ Integer.rotateRight(w[i - 15], 18) //
-            ^ Integer.rotateRight(w[i - 15], 3); //
+            ^ (w[i - 15] >>> 3); //
 
         int s1 = Integer.rotateRight(w[i - 2], 17) //
             ^ Integer.rotateRight(w[i - 2], 19) //
-            ^ Integer.rotateRight(w[i - 2], 10); //
+            ^ (w[i - 2] >>> 10); //
 
         w[i] = w[i - 16] + s0 + w[i - 7] + s1;
-
       }
 
-      // Initialize working variables to current hash value:
-      int a = H[0];
-      int b = H[1];
-      int c = H[2];
-      int d = H[3];
-      int e = H[4];
-      int f = H[5];
-      int g = H[6];
-      int h = H[7];
+      // Initialize working variables a...h to current hash value:
+      int a = hash[0];
+      int b = hash[1];
+      int c = hash[2];
+      int d = hash[3];
+      int e = hash[4];
+      int f = hash[5];
+      int g = hash[6];
+      int h = hash[7];
 
       // Compression function main loop:
       for (int i = 0; i <= 63; i++) {
-        /**
-         *       S1 := (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)
-         *       ch := (e and f) xor ((not e) and g)
-         *       temp1 := h + S1 + ch + k[i] + w[i]
-         *       S0 := (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
-         *       maj := (a and b) xor (a and c) xor (b and c)
-         *       temp2 := S0 + maj
-         *        
-         *       h := g
-         *       g := f
-         *       f := e
-         *       e := d + temp1
-         *       d := c
-         *       c := b
-         *       b := a
-         *       a := temp1 + temp2
-         */
 
-        int s1 = Integer.rotateRight(e, 6) //
+        int s1 = Integer.rotateRight(e, 6) //       S1 := (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)
             ^ Integer.rotateRight(e, 11) //
             ^ Integer.rotateRight(e, 25); //
 
-        int ch = (e & f) ^ (~e & g);
+        int ch = (e & f) ^ ((~e) & g); //           ch := (e and f) xor ((not e) and g)
+        int tmp1 = h + s1 + ch + K[i] + w[i];//     temp1 := h + S1 + ch + k[i] + w[i]
 
-//int tmp1 = 
+        int s0 = Integer.rotateRight(a, 2) //       S0 := (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
+            ^ Integer.rotateRight(a, 13) //         
+            ^ Integer.rotateRight(a, 22); //        
 
+        int maj = (a & b) ^ (a & c) ^ (b & c);//    maj := (a and b) xor (a and c) xor (b and c)
+        int tmp2 = s0 + maj; //                     temp2 := S0 + maj
+
+        h = g;
+        g = f;
+        f = e;
+        e = d + tmp1;
+        d = c;
+        c = b;
+        b = a;
+        a = tmp1 + tmp2;
       }
 
+      hash[0] = a + hash[0];
+      hash[1] = b + hash[1];
+      hash[2] = c + hash[2];
+      hash[3] = d + hash[3];
+      hash[4] = e + hash[4];
+      hash[5] = f + hash[5];
+      hash[6] = g + hash[6];
+      hash[7] = h + hash[7];
+
+    } // chnk
+
+    ByteBuffer digest = ByteBuffer.allocate(32);
+
+    for (int i = 0; i < 8; i++) {
+      digest.putInt(hash[i]);
     }
+
+    for (int h0 : hash) {
+      String s1 = Integer.toHexString(h0);
+      System.out.print(s1);
+    }
+
+    System.out.println();
   }
 
 }
