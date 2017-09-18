4b5e5c7500283be2afa5f0a79fc6e21eaa5d4267
Thomas <thomas.matecki@gmail.com>
2017-09-17 22:37:43 -0400

    Nyc Taxi Medallion Cracker

---
+++ src/main/scala/MedallionCracker.scala
@@ -0,0 +1,33 @@
+import com.windsora.crypto.HashAlgorithms
+
+import scala.io.Source
+
+object medallionCracker extends App {
+
+  val fileLocation = sys.env("FILE_LOCATION")
+
+  val inputHashes: Set[String] = Source.fromFile(fileLocation).getLines.toSet
+
+  val alphabet = Range('A', 'Z', 1).map(_.toChar)
+  val digits = Range(0, 9, 1)
+
+
+  val medallions = for {
+    l1 <- alphabet
+    l2 <- alphabet
+    l3 <- alphabet
+    d1 <- digits
+    d2 <- digits
+    d3 <- digits
+  } yield s"$l1$l2$l3$d1$d2$d3"
+
+  medallions.par.foreach(m => {
+
+    val hash = HashAlgorithms.sha256(m.getBytes).foldLeft(new String)((s, i) => s + f"$i%02x")
+
+    if (inputHashes contains hash) {
+      print(s"medallion: $m -> hash: $hash \n")
+    }
+
+  })
+}
